f94f63304e7ee2b538f83d82dd9390893c3d25b7
Satyam <satyamsrivastava202@gmail.com>
2026-06-08 23:12:19 +0530

    Created main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import ctypes
+import sys
+from blocker import load_json
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def request_admin() -> bool:
+    if is_admin():
+        return True
+    else:
+        print("Requesting administrator privileges...")
+        ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        return False
+
+if __name__ == "__main__":
+    if not is_admin():
+        if not request_admin():
+            print("Failed to obtain administrator privileges.")
+            sys.exit(1)
+    
+    data = load_json("data.json")
